86d4ba9b9fe651245f2203ab61952355945d7752
Cameron Morin <morinc35@gmail.com>
2023-10-15 12:40:21 -0400

    adding service-worker, Manifest, and Install logic

---
+++ client/webpack.config.js
@@ -0,0 +1,60 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js'
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Webpack Plugin",
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        seDest: "service-worker.js",
+      }),
+      new WebpackPwaManifest({
+        name: "Jate",
+        short_name: "J.A.T.E",
+        description: "An easy to use text editor!",
+        start_url: "./",
+        publicPath: "./",
+        theme_color: "#e6dd36",
+        background_color: "d4d2d5",
+        icons: [
+          {
+            src: path.resolve('src/images/log.png'),
+            sizes: [96, 128],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+
+      }),
+      
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        
+        
+      ],
+    },
+  };
+};
