bdb3d30287484ddae2d9962c56f70a880402f982
Cameron Morin <morinc35@gmail.com>
2023-10-17 19:50:36 -0400

    service workers debug

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -24,10 +22,11 @@ module.exports = () => {
       }),
       new InjectManifest({
         swSrc: "./src-sw.js",
-        swDest: "service-worker.js",
+        swDest: "src-sw.js",
       }),
       new WebpackPwaManifest({
         name: "Jate",
+        inject: true,
         short_name: "J.A.T.E",
         description: "An easy to use text editor!",
         start_url: "./",
@@ -37,8 +36,8 @@ module.exports = () => {
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128],
-            destination: path.join("assets", "icons"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "logo"),
           },
         ],
 
